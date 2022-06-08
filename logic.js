4192715af95d78cf58d52820d6cfb3977d3cd25b
Fernando Ubuntu <darkblitzrc@gmail.com>
2022-06-08 13:07:16 -0500

    Add simple math operators to logic

---
+++ logic.js
@@ -0,0 +1,15 @@
+const sum = function(a,b){
+    return a+b
+}
+
+const subtract = function(a,b){
+    return a-b
+}
+
+const multipy = function(a,b){
+    return a*b
+}
+
+const divide = function(a,b){
+    return Math.round(a/b*100)/100
+}
