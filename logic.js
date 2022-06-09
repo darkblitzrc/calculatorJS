16adec801ba05589d56f12a505015f59aba6e22c
Fernando Ubuntu <darkblitzrc@gmail.com>
2022-06-09 13:33:30 -0500

    Add working multiply functionality to calculator!
    WOW, a major breakthrough!
    
    When I first started this project, I was not expecting to hit such a wall early on, indeed the hard part so far was figuring out a way to be able to store the 2 sets of numbers (a,b)!
    
    It's easy to store the initial display value in (a) but once you try figuring out how to store them on (b) it gets tricky. I managed to do this by simply keeping count of the click times. I came across the idea of counting the steps necesary to obtain the result. I believe it's 4 step.
    
    First step you input the number you want to operate with, second step is to chose an operator, third step is inputing the new set of numbers and 4th step would be to either press the same operator to obtain the result OR press the result button to obtain the actual result!

--- logic.js
+++ logic.js
@@ -6,10 +6,86 @@ const subtract = function(a,b){
     return a-b
 }
 
-const multipy = function(a,b){
+const multiply = function(a,b){
     return a*b
 }
 
 const divide = function(a,b){
     return Math.round(a/b*100)/100
 }
+
+const operator = function(operator, a, b){
+    return operator(a,b)
+}
+
+const clearCalc = function(){
+    display.textContent = ''
+    displayValue = []
+    valueA = 0
+    valueB = 0
+    click = 0
+}
+
+const clearDisplay = function(){
+    display.textContent = '';
+    displayValue = []
+}
+
+
+let numPad = document.querySelector('.numPad')
+let display = document.querySelector('.display');
+let clear = document.querySelector('.clear');
+let multi = document.querySelector('.multiply');
+let result = document.querySelector('.result')
+
+for (let i=0; i<=9; i++){
+    let div = document.createElement('div')
+    div.className = 'number';
+    div.textContent = i;
+    div.style.height = '90px';
+    div.style.width = '90px';
+    div.style.border = '2px solid black';
+    numPad.appendChild(div);
+}
+
+displayValue = []
+
+valueA = 0
+valueB = 0
+
+
+let body = document.querySelector('.calcBody');
+body.addEventListener('click', (e)=>{
+    if (e.target.className == 'number'){
+        displayValue.push(e.target.textContent)
+        display.textContent = displayValue.join("")
+    }
+
+
+})
+
+click = 0
+
+ multi.addEventListener('click',()=>{
+    if (click == 0){
+        valueA = display.textContent;
+        click++
+    }else if(click == 2){
+        valueB = display.textContent;
+        display.textContent = operator(multiply,valueA, valueB);
+        valueA = display.textContent;
+        click = 1
+    }
+    
+ })
+
+clear.addEventListener('click', ()=>{
+    clearCalc();
+});
+
+numPad.addEventListener('click', ()=>{
+    if (click == 1){
+        clearDisplay();
+        click++
+    }
+})
