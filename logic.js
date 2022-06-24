9f89ebe7b22b5f6269c595e993354c37bdf62505
Fernando Ubuntu <darkblitzrc@gmail.com>
2022-06-23 21:17:42 -0500

    Add working calculator
    Alright folks! This project took many days mainly because I had to take care of some IRL stuff but the core logic of it was done in about 3-4 days. Like I stated in the earlier commits, the main logic behind the program is using steps defined by the user clicks to determine the state in which the calculator is. Also assigned a type for each operator, this way the calculator which operator was last pressed when obtaining a result.
    
    Overall an awesome project and without a doubt the hardest so far!

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 const sum = function(a,b){
-    return a+b
+    return parseFloat(a)+parseFloat(b);
 }
 
 const subtract = function(a,b){
@@ -11,7 +11,12 @@ const multiply = function(a,b){
 }
 
 const divide = function(a,b){
-    return Math.round(a/b*100)/100
+    output = parseFloat(a)/parseFloat(b)
+    if (output > 0.01){
+        return Math.round(output*100)/100
+    }else{
+        return Math.round(output*100000000)/100000000
+    }
 }
 
 const operator = function(operator, a, b){
@@ -21,8 +26,8 @@ const operator = function(operator, a, b){
 const clearCalc = function(){
     display.textContent = ''
     displayValue = []
-    valueA = 0
-    valueB = 0
+    valueA = null
+    valueB = null
     step = 0
 }
 
@@ -31,49 +36,71 @@ const clearDisplay = function(){
     displayValue = []
 }
 
+const expo = function (x, f = 2) {
+    return Number.parseFloat(x).toExponential(f);
+  }
 
 let numPad = document.querySelector('.numPad')
 let display = document.querySelector('.display');
-let clear = document.querySelector('.clear');
+let clearAll = document.querySelector('.clearAll');
 let multiplyButton = document.querySelector('.multiply');
 let divideButton = document.querySelector('.divide');
-let result = document.querySelector('.result')
+let sumButton = document.querySelector('.sum');
+let subsButton = document.querySelector('.substract');
+let result = document.querySelector('.result');
+let sign = document.querySelector('.signs');
+let percentage = document.querySelector('.percentage');
+let decimal = document.querySelector('.decimal');
 
-for (let i=0; i<=9; i++){
-    let div = document.createElement('div')
-    div.className = 'number';
-    div.textContent = i;
-    div.style.height = '90px';
-    div.style.width = '90px';
-    div.style.border = '2px solid black';
-    numPad.appendChild(div);
-}
 
 displayValue = []
 
-valueA = 0
-valueB = 0
+valueA = null
+valueB = null
 
 step = 0
 type = 0 //1 = multiplication, 2 = division, 3 = sum, 4 = substraction, 5 = result 
 
-let body = document.querySelector('.calcBody');
-body.addEventListener('click', (e)=>{
-    if (e.target.className == 'number'){
-        displayValue.push(e.target.textContent)
-        display.textContent = displayValue.join("")
-    }
+display.style.fontFamily = 'Arial'
 
+let numbersRaw = document.querySelectorAll('.number')
+let numbers = Array.from(numbersRaw);
+numbers.forEach(number => {
+    number.addEventListener('click',(e)=>{
+        if (display.textContent.length <= 8){
+            if (valueA == null & valueB == null){
+                displayValue.push(e.target.textContent)
+                display.textContent = displayValue.join("")
+                step = 1
+                }
+            if (step == 2){
+                    clearDisplay();
+                    step = 3
+            }
+            if(step == 3){
+                    displayValue.push(e.target.textContent)
+                    display.textContent = displayValue.join("")
+            }
 
-})
+        }
+    })
+});
 
  multiplyButton.addEventListener('click',()=>{
     if (type != 1){
-        if(type == 2 && valueB == 0){
-            step = 2
-        }else if(type == 2){
+        if(type == 2){
             valueB = display.textContent;
             display.textContent = operator(divide,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 3){
+            valueB = display.textContent;
+            display.textContent = operator(sum,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 4){
+            valueB = display.textContent;
+            display.textContent = operator(subtract,valueA, valueB);
             valueA = display.textContent;
             step = 2
         }else if (type == 5){
@@ -97,11 +124,19 @@ body.addEventListener('click', (e)=>{
 
 divideButton.addEventListener('click', ()=>{
     if (type != 2){
-        if(type == 1 && valueB == 0){
-            step = 2
-        }if(type == 1){
+        if(type == 1){
             valueB = display.textContent;
             display.textContent = operator(multiply,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 3){
+            valueB = display.textContent;
+            display.textContent = operator(sum,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 4){
+            valueB = display.textContent;
+            display.textContent = operator(subtract,valueA, valueB);
             valueA = display.textContent;
             step = 2
         }else if (type == 5){
@@ -123,36 +158,144 @@ divideButton.addEventListener('click', ()=>{
     type = 2
 })
 
-clear.addEventListener('click', ()=>{
+sumButton.addEventListener('click', ()=>{
+    if (type != 3){
+        if(type == 1){
+            valueB = display.textContent;
+            display.textContent = operator(multiply,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 2){
+            valueB = display.textContent;
+            display.textContent = operator(divide,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 4){
+            valueB = display.textContent;
+            display.textContent = operator(subtract,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if (type == 5){
+            valueA = display.textContent;
+            step = 2
+        }
+    }
+
+        if(step == 1){
+            valueA =  display.textContent;
+            step = 2
+        }else if(step == 3){
+            valueB = display.textContent;
+            display.textContent = operator(sum,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }
+        
+    type = 3
+})
+
+subsButton.addEventListener('click', ()=>{
+    if (type != 4){
+        if(type == 1){
+            valueB = display.textContent;
+            display.textContent = operator(multiply,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 2){
+            valueB = display.textContent;
+            display.textContent = operator(divide,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if(type == 3){
+            valueB = display.textContent;
+            display.textContent = operator(sum,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }else if (type == 5){
+            valueA = display.textContent;
+            step = 2
+        }
+    }
+
+        if(step == 1){
+            valueA =  display.textContent;
+            step = 2
+        }else if(step == 3){
+            valueB = display.textContent;
+            display.textContent = operator(subtract,valueA, valueB);
+            valueA = display.textContent;
+            step = 2
+        }
+        
+    type = 4
+})
+
+clearAll.addEventListener('click', ()=>{
     clearCalc();
+    display.style.fontSize = '40px'
 });
 
-numPad.addEventListener('click', ()=>{
-    if (valueA == 0 & valueB == 0){
-        step = 1
-    }else if (step == 2){
-        clearDisplay();
-        step = 3
-    }
-}) 
 
 result.addEventListener('click',()=>{
-    if(type == 1 & valueB == 0){
+    if(type == 1 & valueB == null){
         valueB = display.textContent;
         display.textContent = operator(multiply,valueA, valueB);
         valueA = display.textContent;
-        valueB = 0;
     }else if(type == 1){
-        valueB = display.textContent;
+        valueB = display.textContent; 
         display.textContent = operator(multiply,valueA, valueB);
-    }else if(type == 2 & valueB == 0){
-        valueB = display.textContent;
-        display.textContent = operator(divide,valueA, valueB);
-        valueA = display.textContent;
+    }else if(type == 2 & valueB == null){
+        valueB = display.textContent; 
+        if (valueA != 0 && valueB == 0){
+            display.style.fontSize = '18px'
+            display.textContent = 'Error: Can\'t divide by zero.'
+        }else{
+            display.textContent = operator(divide,valueA, valueB);
+            valueA = display.textContent; 
+        }
     }else if(type == 2){
+        valueB = display.textContent; 
+        display.textContent = operator(divide,valueA, valueB);  
+    }else if(type == 3 & valueB == null){
         valueB = display.textContent;
-        display.textContent = operator(divide,valueA, valueB);
-        valueA = display.textContent;      
+        display.textContent = operator(sum,valueA, valueB);
+        valueA = display.textContent;
+    }else if(type == 3){
+        valueB = display.textContent; 
+        display.textContent = operator(sum,valueA, valueB);
+    }else if(type == 4 & valueB == null){
+        valueB = display.textContent;
+        display.textContent = operator(subtract,valueA, valueB);
+        valueA = display.textContent;
+    }else if(type == 4){
+        valueB = display.textContent; 
+        display.textContent = operator(subtract,valueA, valueB);
+        
     }
-    type = 5
+  type = 5
+})
+
+sign.addEventListener('click', ()=>{
+    if (displayValue[0] != '-'){
+        displayValue.unshift('-');
+        display.textContent = displayValue.join('');
+    }else if (displayValue[0] == '-'){
+        displayValue.shift()
+        display.textContent = displayValue.join('');
+    }
+})
+
+percentage.addEventListener('click',()=>{
+    percent = display.textContent/100
+    display.textContent = percent
+})
+
+decimal.addEventListener('click', ()=>{
+    let find = displayValue.find((decimal)=>{
+        return decimal == '.'
+    })
+    if(find == undefined){
+        displayValue.push('.');
+        display.textContent = displayValue.join('');
+    }
 })
